a70a2c6f0883f2c583537433b9e7c9a4b3413008
Datlxthe180538 <datlxthe180538@fpt.edu.vn>
2024-09-23 15:53:18 +0700

    Update : dashboard.java

--- src/java/control/dashboard.java
+++ src/java/control/dashboard.java
@@ -2,31 +2,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package control;
 
+import dal.DashboardDAO;
+import dto.ChartDTO;
+import dto.StasticDto;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  *
  * @author nhatk
  */
 public class dashboard extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 //        try (PrintWriter out = response.getWriter()) {
 //            /* TODO output your page here. You may use following sample code. */
@@ -40,12 +44,20 @@ public class dashboard extends HttpServlet {
 //            out.println("</body>");
 //            out.println("</html>");
 //        }
-            response.sendRedirect("dashboard.jsp");
-    } 
+        DashboardDAO dashboardDAO = new DashboardDAO();
+        StasticDto dto = dashboardDAO.getStasticDto();
+        List<ChartDTO> chartDTOs = dashboardDAO.getData();
+        request.setAttribute("dto", dto);
+        for(ChartDTO item : chartDTOs){
+            request.setAttribute("month" + item.getMonth(), item.getTotal());
+        }
+        request.getRequestDispatcher("dashboard.jsp").forward(request, response);
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -53,12 +65,13 @@ public class dashboard extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -66,12 +79,13 @@ public class dashboard extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
