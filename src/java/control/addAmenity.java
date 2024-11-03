2c4e56cb33decdd27bb29db4c2c918425cac65de
Datlxthe180538 <datlxthe180538@fpt.edu.vn>
2024-11-03 17:22:44 +0700

    validate input

--- src/java/control/addAmenity.java
+++ src/java/control/addAmenity.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package control;
 
 import dal.AmenityDAO;
@@ -23,37 +22,50 @@ import model.Service;
  * @author admin
  */
 public class addAmenity extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-        HttpSession session = request.getSession();
-        AmenityDAO adao = new AmenityDAO();
-        String name = request.getParameter("name");
-        Amenity a = new Amenity();
-        a.setAmenName(name);
-        
-        adao.addAmenity(a);
-        List<Amenity> listAmenity = adao.getAllAmenities();
-        session.setAttribute("listAmenity", listAmenity);
-        request.getRequestDispatcher("listAmenity.jsp").forward(request, response);
+            HttpSession session = request.getSession();
+            AmenityDAO adao = new AmenityDAO();
+            List<Amenity> listAmenity = adao.getAllAmenities();
+            String name = request.getParameter("name").trim();
+            boolean flag = false;
+            for (Amenity a : listAmenity) {
+                if (a.getAmenName().equals(name)) {
+                    request.setAttribute("duplicate", "Amenity existed in system.");
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                Amenity a = new Amenity();
+                a.setAmenName(name);
+
+                adao.addAmenity(a);
+            }
+            session.setAttribute("listAmenity", listAmenity);
+            request.getRequestDispatcher("listAmenity.jsp").forward(request, response);
         } catch (ServletException | IOException | NumberFormatException e) {
             out.print(e.getMessage());
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,12 +73,13 @@ public class addAmenity extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -74,12 +87,13 @@ public class addAmenity extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
